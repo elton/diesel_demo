04bc496942d39d81bd2cb4ef4702f6cb441fec18
Elton Zheng <elton.zheng.sh@gmail.com>
2020-10-20 21:49:51 +0800

    Add Delete function.

---
+++ src/bin/delete_post.rs
@@ -0,0 +1,33 @@
+// Copyright 2020 Elton Zheng
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+extern crate diesel;
+extern crate diesel_demo;
+
+use self::diesel::prelude::*;
+use self::diesel_demo::*;
+use std::env::args;
+
+fn main() {
+    use diesel_demo::schema::posts::dsl::*;
+
+    let target = args().nth(1).expect("Expected a target to match against");
+    let pattern = format!("%{}%", target);
+
+    let connection = establish_connection();
+    let num_deleted = diesel::delete(posts.filter(title.like(pattern)))
+        .execute(&connection)
+        .expect("Error deleting posts");
+
+    println!("Delete {} posts.", num_deleted);
+}
