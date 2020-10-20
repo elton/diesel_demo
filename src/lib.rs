a13a2e1181d0a7c19f18ce5cec0059406e2649a3
Elton Zheng <elton.zheng.sh@gmail.com>
2020-10-20 21:29:05 +0800

    Add Create function.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+// marco_use 让 schema.rs中的宏命令起作用
 #[macro_use]
 extern crate diesel;
 extern crate dotenv;
@@ -15,4 +16,19 @@ pub fn establish_connection() -> PgConnection {
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+}
+
+use self::models::{NewPost, Post};
+
+pub fn create_post<'a>(conn: &PgConnection, title: &'a str, body: &'a str) -> Post {
+    use schema::posts;
+
+    let new_post = NewPost { title, body };
+
+    diesel::insert_into(posts::table)
+        .values(&new_post)
+        // 插入一个记录就用.get_result, 如插入多条记录，传入的就是Vec，使用.get_results替代get_result
+        // 如果不想得到插入后返回的内容，就用.execute
+        .get_result(conn)
+        .expect("Error saving new post")
 }
